2e221228452eb4aa4d5990cc0dc56de9c1e02e69
sreejith.k <sreejith.k@beehyv.com>
2023-08-24 18:35:49 +0530

    undo changes in config files

--- gateway/onest-ui/src/config/config.js
+++ gateway/onest-ui/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // BackendURL: process.env.REACT_APP_BACKEND_URL 
-    BackendURL: "http://localhost:8000",
+    BackendURL: process.env.REACT_APP_BACKEND_URL 
+    // BackendURL: "http://localhost:8000",
 }
 
 export default config;
